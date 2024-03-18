aba63caa6e8e2ce58b326e15f2b8327819c4e7dc
kurt <kurt.cheong@bertelsmann.de>
2024-03-18 14:13:00 +0800

    [PetTracker] add constant to localhost for reuse

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,36 +2,39 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Owner } from "../owner/owner.model";
 import { Pet } from "../pets/pet.model";
+
+const LOCALHOST: string = 'http://localhost:8080';
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
+
     constructor(private http: HttpClient) { }
     fetchOwners() {
         return this.http
             .get<Owner[]>(
-                'http://localhost:8080/owners'
+                LOCALHOST + '/owners'
             )
     }
 
     storeOwner(owner: Owner) {
-        return this.http.post<Owner>('http://localhost:8080/owner', owner);
+        return this.http.post<Owner>(LOCALHOST+'/owner', owner);
     }
 
     fetchOwnerPets(id: number) {
         return this.http
             .get<Pet[]>(
-                'http://localhost:8080/pet/owner/' + id
+                LOCALHOST+'/pet/owner/' + id
             );
     }
 
     storePet(pet: Pet, id: number) {
-        return this.http.post<Pet>('http://localhost:8080/pet/' + id, pet);
+        return this.http.post<Pet>(LOCALHOST+'/pet/' + id, pet);
     }
 
     putPet(pet: Pet) {
-        return this.http.put('http://localhost:8080/pet/' + pet.id, pet);
+        return this.http.put(LOCALHOST+'/pet/' + pet.id, pet);
     }
 
     removePet(id: number) {
-        return this.http.delete('http://localhost:8080/pet/' + id);
+        return this.http.delete( LOCALHOST+'/pet/' + id);
     }
 }
